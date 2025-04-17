4d76ddd2451b9511427c1615c08c4bf4bf4631af
studiorosalind <studiorosalind@gmail.com>
2025-04-17 09:10:00 +0900

    modify gradle config

--- rosalind-api/src/main/java/com/rosalind/api/RosalindApiApplication.java
+++ rosalind-api/src/main/java/com/rosalind/api/RosalindApiApplication.java
@@ -4,12 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.rosalind.api", "com.rosalind.common"})
 @EntityScan("com.rosalind.common.domain")
-@EnableJpaRepositories("com.rosalind.common.repository")
 public class RosalindApiApplication {
 
     public static void main(String[] args) {
--- rosalind-common/src/main/java/com/rosalind/common/domain/ERole.java
+++
@@ -1,7 +0,0 @@
-package com.rosalind.common.domain;
-
-public enum ERole {
-    ROLE_USER,
-    ROLE_MODERATOR,
-    ROLE_ADMIN
-}
